0ae149f19af56f46f8f1a286bdf99604a9d01624
Caio <caio.almeidabeleza@gmail.com>
2021-04-12 01:12:43 -0300

    Generating json with number from request

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # N-mero-por-Extenso
+
+Rodando a aplicação pela primeira vez:
+
+sudo docker-compose build
+
+sudo docker-compose up
+
+Ao iniciar:
+http://0.0.0.0:5000/<numero inteiro para transformar em extenso>
--- docker/api/app.py
+++ docker/api/app.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask, jsonify
+from extenso import real
 app = Flask(__name__)
 
 #we define the route /
-@app.route('/')
-def welcome():
+@app.route('/<int:num>', methods=["GET"])
+def get_extenso(num):
+    extenso = real(num)
     # return a json
-    return jsonify({'status': 'api working'})
+    return jsonify({'extenso': extenso})
 
 if __name__ == '__main__':
     #define the localhost ip and the port that is going to be used
